d6ff5724a61ca12a39cebf4753c2b8e27941c100
malak-ehab <169089163+malak-ehab@users.noreply.github.com>
2024-10-15 02:39:41 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
 
 function myFunction(){
 let x = document.getElementById("cel").value;
+  if (x === ""){
+document.getElementById("result").innerHTML ="Enter a number";
+}
+  else{
 let y = (x * (9/5)) + 32;
 const c = "Temperature in fahrenheit is:"
 document.getElementById("result").innerHTML = c + "   " + "  " + y;
+}
 }
